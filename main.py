98769e5a756a31042dd07b614d747142643e7e12
apl26 <apl2612@mail.ru>
2023-02-12 01:23:08 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import math
+import tkinter as tk
+
+
+def pyshader(func, w, h):
+    scr = bytearray((0, 0, 0) * w * h)
+    for y in range(h):
+        for x in range(w):
+            p = (w * y + x) * 3
+            scr[p:p + 3] = [max(min(int(c * 255), 255), 0)
+                            for c in func(x / w, y / h)]
+    return bytes('P6\n%d %d\n255\n' % (w, h), 'ascii') + scr
+
+
+# Ваш код здесь:
+def func(x, y):
+    if (2 * x - 1)**2 + 0.2 > y or -(2 * x - 1)**2 + 0.8 < y:
+        return 256, 256, 256
+    else:
+        return x, y, 0
+
+
+label = tk.Label()
+img = tk.PhotoImage(data=pyshader(func, 256, 256)).zoom(2, 2)
+label.pack()
+label.config(image=img)
+tk.mainloop()
